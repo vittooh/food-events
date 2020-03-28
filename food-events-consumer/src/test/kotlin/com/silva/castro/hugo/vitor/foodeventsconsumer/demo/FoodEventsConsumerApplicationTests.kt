fd4dfc31ed1aba5c70f5bdcc94a59e6dc566eb74
vitor <vitorhugosilva2009@hotmail.com>
2020-03-28 15:01:36 -0300

    refatorando estrutura do projeto.

---
+++ food-events-consumer/src/test/kotlin/com/silva/castro/hugo/vitor/foodeventsconsumer/demo/FoodEventsConsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.silva.castro.hugo.vitor.foodeventsconsumer.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodEventsConsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
